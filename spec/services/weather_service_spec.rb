a4037495914266114415c8672a4ae2a787ebd249
Aniket Harde <hardeaniket8@gmail.com>
2025-06-27 16:02:37 +0530

    test: add spec for WeatherService caching behavior (currently failing)
    - Adds a test to verify WeatherService caches the forecast for 30 minutes by zip code
    - Test fails as caching is not yet implemented; HTTParty.get is called more than once
    - Committing failing spec as part of TDD
    
    Changes to be committed:
    	modified:   spec/services/weather_service_spec.rb

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe WeatherService do
       expect(result[:temperature]).to be_a(Numeric)
     end
   end
+
+  describe "caching behavior", :vcr do
+    it "caches the forecast for 30 minutes by zip code" do
+      Rails.cache.clear
+
+      # First call
+      result_1 = service.get_forecast
+
+      allow(HTTParty).to receive(:get).and_call_original
+
+      # Second call - should use cache, so HTTParty.get should not be called
+      result_2 = service.get_forecast
+
+      expect(result_2).to eq(result_1)
+      expect(HTTParty).to have_received(:get).once
+    end
+  end
 end
