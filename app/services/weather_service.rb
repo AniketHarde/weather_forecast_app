c87fc11d4da479ba26c8fe25562aabe860fa46aa
Aniket Harde <hardeaniket8@gmail.com>
2025-06-27 16:54:17 +0530

    feat: add caching to WeatherService#get_forecast
    - Implements Rails.cache to store weather forecast by zip code for 30 minutes
    - Reduces redundant API calls
    
     Changes to be committed:
    	modified:   app/services/weather_service.rb
    	new file:   spec/vcr_cassettes/WeatherService/caching_behavior/caches_the_forecast_for_30_minutes_by_zip_code.yml

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -10,18 +10,21 @@ class WeatherService
   end
 
   def get_forecast
-    response = HTTParty.get(BASE_URL, query: {
-      zip: "#{@zip_code},#{@country_code}",
-      units: "metric",
-      appid: @api_key
-    })
+    cache_key = "forecast:zip:#{@zip_code}"
+    Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
+      response = HTTParty.get(BASE_URL, query: {
+        zip: "#{@zip_code},#{@country_code}",
+        units: "metric",
+        appid: @api_key
+      })
 
-    if response.success?
-      {
-        temperature: response["main"]["temp"]
-      }
-    else
-      raise StandardError, "Failed to fetch forecast: #{response.code}"
+      if response.success?
+        {
+          temperature: response["main"]["temp"]
+        }
+      else
+        raise StandardError, "Failed to fetch forecast: #{response.code}"
+      end
     end
   end
 end
