c466e918d8bc1ba4b711ab480e519c9fa972f783
Aniket Harde <hardeaniket8@gmail.com>
2025-06-27 13:30:00 +0530

    Failing spec: WeatherService not autoloaded, uninitialized constant error
    - Added WeatherService spec for #get_forecast method
    - Spec fails with NameError: uninitialized constant WeatherService
    - Following TDD: committing failing test before implementing/fixing service

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe WeatherService do
+  let(:zip_code) { '452009' } # Indore ZIP
+
+  subject(:service) { described_class.new(zip_code) }
+
+  describe "#get_forecast" do
+    it "returns the current temperature for a valid zip code" do
+      result = service.get_forecast
+
+      expect(result).to include(:temperature)
+      expect(result[:temperature]).to be_a(Numeric)
+    end
+  end
+end
