5e4b49b81af8b28a831af78f6abdd0af0fef09fd
Aniket Harde <hardeaniket8@gmail.com>
2025-06-27 17:36:34 +0530

    test: add specs for from_cache flag in WeatherService#get_forecast (failing)
    - Adds tests to verify :from_cache flag is present and correct in get_forecast results
    - Tests expect :from_cache to be false on API hit and true when data is cached
    - Both tests currently fail as :from_cache is not implemented in the service
    - Committing failing specs as part
    - and updated older code to support this flag

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -12,6 +12,26 @@ RSpec.describe WeatherService do
       expect(result).to include(:temperature)
       expect(result[:temperature]).to be_a(Numeric)
     end
+
+    it "returns from_cache: false when API is hit" do
+      Rails.cache.clear
+      result = service.get_forecast
+
+      expect(result).to include(:from_cache)
+      expect(result[:from_cache]).to eq(false)
+    end
+
+    it "returns from_cache: true when data is cached" do
+      Rails.cache.clear
+
+      # First call caches the result
+      service.get_forecast
+
+      # Second call should return cached data
+      result = service.get_forecast
+
+      expect(result[:from_cache]).to eq(true)
+    end
   end
 
   describe "caching behavior", :vcr do
@@ -26,8 +46,8 @@ RSpec.describe WeatherService do
       # Second call - should use cache, so HTTParty.get should not be called
       result_2 = service.get_forecast
 
-      expect(result_2).to eq(result_1)
-      expect(HTTParty).to have_received(:get).once
+      expect(result_2[:temperature]).to eq(result_1[:temperature])
+      expect(HTTParty).not_to have_received(:get)
     end
   end
 end
