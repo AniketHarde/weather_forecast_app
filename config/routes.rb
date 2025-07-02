f0a2d60356b4b6cef8630ce9373bd0dc52eb2081
Aniket Harde <hardeaniket8@gmail.com>
2025-07-02 16:24:44 +0530

    Add routes for clear cache and clear history

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "weather#index"
-  post "weather/show", to: "weather#show"
+  post "weather/show", to: "weather#show", as: :weather_show
+  get  'weather/show', to: 'weather#show'
+  delete 'weather/clear_cache', to: 'weather#clear_cache'
+  delete 'weather/clear_history', to: 'weather#clear_history'
 end
